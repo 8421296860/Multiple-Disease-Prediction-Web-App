0261bd7b256d7feabcf55f77b394be77c543a22e
BHUSHAN SATISH DESHMUKH <deshmukhbhushan2001@gmail.com>
2022-09-09 14:15:17 +0530

    fixed windows path issue

--- multiple disease pred.py
+++ multiple disease pred.py
@@ -5,11 +5,11 @@ from streamlit_option_menu import option_menu
 
 # loading the saved models
 
-diabetes_model = pickle.load(open('C:\Users\Bhushan Deshmukh\Downloads\Multiple Disease Prediction System-20220906T170039Z-001\Multiple Disease Prediction System\saved models\diabetes_model.sav', 'rb'))
+diabetes_model = pickle.load(open('saved models/diabetes_model.sav', 'rb'))
 
-heart_disease_model = pickle.load(open('C:\Users\Bhushan Deshmukh\Downloads\Multiple Disease Prediction System-20220906T170039Z-001\Multiple Disease Prediction System\saved models\heart_disease_model.sav','rb'))
+heart_disease_model = pickle.load(open('saved models/heart_disease_model.sav','rb'))
 
-parkinsons_model = pickle.load(open('C:\Users\Bhushan Deshmukh\Downloads\Multiple Disease Prediction System-20220906T170039Z-001\Multiple Disease Prediction System\saved models\parkinsons_model.sav', 'rb'))
+parkinsons_model = pickle.load(open('saved models/parkinsons_model.sav', 'rb'))
 
 
 
